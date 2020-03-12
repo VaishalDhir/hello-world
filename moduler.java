d495a286d674e36e43776abba7588cf3de34452b
Virk <2091427@MATRIXCOLLEGE.LCL>
2020-03-12 13:01:07 -0700

    my country

---
+++ moduler.java
@@ -0,0 +1,22 @@
+package project;
+
+import java.util.Scanner;
+
+public class moduler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("enter any no.");
+		int y = sc.nextInt();
+if ((y%2)==0)
+{
+System.out.println("print the number is even");
+	}
+else {
+	System.out.println("print the number is odd");
+}
+	}
+	
+}
